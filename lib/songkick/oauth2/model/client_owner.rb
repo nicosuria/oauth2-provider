994ecadd6aa384f0d4aaaa50844ea8b8d4e65627
Nico Suria <nsuria@payrollhero.com>
2012-11-13 14:15:09 +0800

    Removed ActiveRecord references

--- lib/songkick/oauth2/model/client_owner.rb
+++ lib/songkick/oauth2/model/client_owner.rb
@@ -1,15 +1,17 @@
-module Songkick
-  module OAuth2
-    module Model
-      
-      module ClientOwner
-        def self.included(klass)
-          klass.has_many :oauth2_clients,
-                         :class_name => 'Songkick::OAuth2::Model::Client',
-                         :as => :oauth2_client_owner
-        end
-      end
-      
-    end
-  end
-end
+# TODO: Implement this as a DataMapper resource
+#
+# module Songkick
+#   module OAuth2
+#     module Model
+#       
+#       module ClientOwner
+#         def self.included(klass)
+#           klass.has_many :oauth2_clients,
+#                          :class_name => 'Songkick::OAuth2::Model::Client',
+#                          :as => :oauth2_client_owner
+#         end
+#       end
+#       
+#     end
+#   end
+# end
